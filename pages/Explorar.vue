9eeb749f479453a5d24466bd6216402b1094b735
pjdu39 <pjdu39@gmail.com>
2022-09-18 21:00:21 +0200

    Refactor de css y efectos con hover

--- pages/Explorar.vue
+++ pages/Explorar.vue
@@ -1,71 +1,106 @@
 <template>
-  <div class="container">
-    <b-list-group-item class="image-card" v-for="receta in recetas" :key="receta">
-        {{ receta.text }}
-        <img class="image" :src="receta.text">
-    </b-list-group-item>
+  <div>
+    <b-row>
+      <b-col class="col-md-2"></b-col>
+      <b-col class="col-md-8 images-container">
+        <b-list-group-item class="card" v-for="receta in recetas" :key="receta">
+          <div class="card-tittle" :hidden="muestraTitulo">
+            {{ receta.name }}
+          </div>
+          <a href="#" class="card-link">
+            <!-- @mouseover="hover(receta.name)  Evento de hover -->
+            <img class="card-image" :src="receta.img" />
+          </a>
+        </b-list-group-item>
+      </b-col>
+      <b-col class="col-md-2"></b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            recetas: [
-                { text: require("~/assets/img/receta-lentejas.jpg") },
-                { text: "Receta 2"},
-                { text: "Receta 3"},
-                { text: "Receta 4"},
-                { text: "Receta 5"},
-                { text: "Receta 6"},
-                { text: "Receta 7"},
-                { text: "Receta 8"},
-                { text: "Receta 9"},
-                { text: "Receta 10"},
-                { text: "Receta 11"},
-                { text: "Receta 12"},
-                { text: "Receta 13"},
-                { text: "Receta 14"},
-                { text: "Receta 15"},
-                { text: "Receta 16"},
-                { text: "Receta 17"},
-                { text: "Receta 18"},
-                { text: "Receta 19"},
-                { text: "Receta 20"},
-                { text: "Receta 21"}
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      muestraTitulo: false,
+      tituloRecetaHover: null,
+      recetas: [
+        { name: "Cocido", img: require("~/assets/img/cocido.jpg") },
+        { name: "Postre", img: require("~/assets/img/postre1.jpg") },
+        { name: "Cóctel", img: require("~/assets/img/coctel1.jpg") },
+        { name: "Pizza", img: require("~/assets/img/pizza.jpg") },
+        { name: "Cocido", img: require("~/assets/img/cocido.jpg") },
+        { name: "Postre", img: require("~/assets/img/postre1.jpg") },
+        { name: "Cóctel", img: require("~/assets/img/coctel1.jpg") },
+        { name: "Pizza", img: require("~/assets/img/pizza.jpg") },
+        { name: "Cocido", img: require("~/assets/img/cocido.jpg") },
+        { name: "Postre", img: require("~/assets/img/postre1.jpg") },
+        { name: "Cóctel", img: require("~/assets/img/coctel1.jpg") },
+        { name: "Pizza", img: require("~/assets/img/pizza.jpg") },
+        { name: "Cocido", img: require("~/assets/img/cocido.jpg") },
+        { name: "Postre", img: require("~/assets/img/postre1.jpg") },
+        { name: "Cóctel", img: require("~/assets/img/coctel1.jpg") }
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
-.container {
-    max-width: 100%;
-    display: grid;
-    grid-template-columns: repeat(6, 1fr);
-    /* grid-template-rows: repeat(auto-fill, 200px); */ /* Averiguar qué es esto y por qué solo se está aplicando a la primera row */
-    grid-row-gap: 0;
-    grid-column-gap: 0;
-    overflow: auto;
+.images-container {
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* grid-template-rows: repeat(auto-fill, 200px); */ /* Averiguar qué es esto y por qué solo se está aplicando a la primera row */
+  row-gap: 0;
+  column-gap: 0;
+  overflow: auto;
 }
-.image-card {
-    width: 14vw;
-    height: 14vw;
-    margin-top: 1rem;
-    margin-right: 1rem;
-    margin-bottom: 1rem;
-    padding: 3rem;
-    background-color: #eaedee;
-    border-style: none;
-    border-radius: 0.8rem;
-    box-shadow: 1.5px 1.5px 23px -10px #252B31;
+.card {
+  width: 12vw;
+  height: 12vw;
+  margin-top: 1rem;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+  border-style: none;
+  border-radius: 0.8rem;
+  box-shadow: 1.5px 1.5px 23px -10px #252b31;
+  overflow: hidden;
 }
-.image {
-    top: 0;
-    left: 0;
-    width: inherit;
-    height: inherit;
-    position: absolute;
+.card:hover {
+    
+  width: 12.2vw;
+  height: 12.2vw;
+  margin-right: 0rem;
+  margin-bottom: 0rem;
+  box-shadow: 1.5px 1.5px 8px 6px #252b31;
+}
+.card-tittle {
+  position: absolute;
+  z-index: 5;
+  padding: 0 1rem;
+  border-radius: 0.8rem;
+  color: #eaedee;
+  background-color: #252b31;
+  box-shadow: 2px 2px 12px 0px #252b31;
+  border-style: none;
+  font-weight: 600;
+}
+.card-link {
+  padding: auto;
+  display: block;
+  width: inherit;
+  height: inherit;
+}
+.card-image {
+  top: 0;
+  left: 0;
+  width: auto;
+  height: inherit;
+  position: absolute;
+  z-index: 10;
+}
+.card-image:hover {
+  z-index: 1;
 }
 </style>
