7c3b6e6d667bedac4a3ccbae6cb15f8a22422b54
Pablo Javier Duarte Ubeda <pjdu39@gmail.com>
2022-09-23 22:28:24 +0200

    Perfil y filtros de búsqueda maquetados en la vista Recetas

--- pages/Receta.vue
+++ pages/Receta.vue
@@ -1,15 +1,35 @@
 <template>
   <div>
     <b-row>
-      <b-col class="col-md-3 box box1">
-        <div class="perfil-container">
-          <div class="wrapper-foto">
-            <img class="perfil-foto" :src="usuarioDummy.img">
-          </div>
-          <div class="perfil-info">{{ usuarioDummy.publicaciones }}</div>
-          <div class="perfil-info">{{ usuarioDummy.seguidores }}</div>
-          <div>{{ usuarioDummy.nombreCompleto }}</div>
-        </div>
+      <b-col class="col-md-3 box perfil-container">
+        <b-row class="perfil-seccion1">
+          <b-col class="col-md-4 perfil-foto">
+            <div class="wrapper-foto">
+              <img class="foto" :src="usuarioDummy.img" />
+              <!-- TODO: Ponerle un link al perfil -->
+            </div>
+          </b-col>
+          <b-col class="col-md-4 perfil-info">
+            <div class="perfil-info-box">
+              <h6 class="perfil-info-num">{{ usuarioDummy.seguidores }}</h6>
+              <div class="perfil-info-lit">Seguidores</div>
+            </div>
+          </b-col>
+          <b-col class="col-md-4 perfil-info">
+            <div class="perfil-info-box">
+              <h6 class="perfil-info-num">
+                {{ usuarioDummy.publicaciones }}
+              </h6>
+              <div class="perfil-info-lit">Recetas</div>
+            </div>
+          </b-col>
+        </b-row>
+        <div>{{ usuarioDummy.nombreCompleto }}</div>
+        <p>
+          CHEF de @nakedandsated THE BEST FODDIE 2018 AUTOR DE 3 LIBROS
+          CONSEJERO MCHEF VI
+          <a href="#">linktr.ee/chefbosquet</a>
+        </p>
       </b-col>
       <b-col class="col-md-6 box box2">
         <h1 class="title-receta">Receta</h1>
@@ -51,16 +71,33 @@
         </p>
       </b-col>
       <b-col class="col-md-3 box box3">
-        <div>
-          <b-form-input v-model="buscar" class="buscador" placeholder="Buscar..."></b-form-input>
+        <div class="buscador-card">
+          <b-form-input
+            v-model="buscar"
+            class="buscador"
+            placeholder="Buscar..."
+          ></b-form-input>
           <b-list-group-item
             class="badge-container"
             v-for="result in busquedaResultado"
             :key="result"
           >
             <!-- Considerar utilizar Badge de bootstrap-vue para mostrar los ingredientes seleccionados. -->
-            <a class="badge-custom">
+            <a class="badge-custom" @click="AddFilter(result)">
               {{ result.literal }}
+            </a>
+          </b-list-group-item>
+        </div>
+        <div class="buscador-card buscador-card--filtro">
+          <h6>Filtros:</h6>
+          <b-list-group-item
+            class="badge-container badge-container--filtro"
+            v-for="filtro in filtros"
+            :key="filtro"
+          >
+            <!-- Considerar utilizar Badge de bootstrap-vue para mostrar los ingredientes seleccionados. -->
+            <a class="badge-custom badge-custom--filtro">
+              {{ filtro.literal }} <!-- Icono de X en un link para quitar el filtro -->
             </a>
           </b-list-group-item>
         </div>
@@ -74,7 +111,6 @@ export default {
   data() {
     return {
       buscar: "",
-      buscar2: "tocino",
       ingredientes: [
         { id: 1, literal: "piña" },
         { id: 2, literal: "tocino" },
@@ -95,15 +131,17 @@ export default {
         { id: 17, literal: "queso chedar" },
         { id: 18, literal: "queso de cabra" },
         { id: 19, literal: "queso parmesano" },
-        { id: 20, literal: "queso mozarella" }
+        { id: 20, literal: "queso mozarella" },
       ],
       usuarioDummy: {
-        nombreCompleto: 'Juan Pérez Delgado',
+        id: 1,
+        nombreCompleto: "Juan Pérez Delgado",
         publicaciones: 28,
         seguidores: 143,
-        img: require("~/assets/img/foto_perfil2.jpg")
-      }
-    }
+        img: require("~/assets/img/foto_perfil2.jpg"),
+      },
+      filtros: [],
+    };
   },
   computed: {
     busquedaResultado() {
@@ -111,50 +149,96 @@ export default {
       return this.ingredientes.filter((x) => x.literal.includes(this.buscar));
     },
   },
+  methods: {
+    AddFilter(filtro) {
+      if (this.filtros.includes(filtro)) {
+        return
+      }
+      this.filtros.push(filtro);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.box1 {
-}
 .perfil-container {
-  margin-left: 6rem;
+  padding: 2.5rem 3rem 0 4rem;
+}
+.perfil-seccion1 {
+  margin-bottom: 2rem;
+}
+.perfil-foto {
+  /*text-align: center;*/
 }
 .wrapper-foto {
   display: inline-block;
-  width: 3.5rem;
-  height: 3.5rem;
+  width: 3.7rem;
+  height: 3.7rem;
   overflow: hidden;
   position: relative;
   border-radius: 50%;
 }
-.perfil-info {
-  text-align: right;
-  float: right;
-}
-.perfil-foto {
+.foto {
   width: auto;
   height: 100%;
-  margin-left: -.4rem;
+  margin-left: -0.4rem;
+  margin-bottom: 2rem;
+}
+.perfil-info {
+  padding-top: 0.6rem;
+}
+.perfil-info-box {
+  display: inline-block;
+  text-align: center;
+}
+.perfil-info-num {
+  font-weight: 700;
+  margin: 0;
+}
+.perfil-info-lit {
+  font-weight: 350;
+  font-size: 90%;
 }
 .box3 {
   padding-right: 5rem;
 }
+.buscador-card {
+  min-height: 18rem;
+  max-height: 18rem;
+  margin-bottom: 2rem;
+  overflow: hidden;
+}
 .buscador {
+  margin-top: 1.5rem;
   margin-bottom: 1.5rem;
 }
 .badge-container {
-  display: inline;
+  display: inline-block;
   padding: 0;
   background-color: transparent;
   border: none;
+  margin-bottom: 0.7rem;
 }
 .badge-custom {
   color: #252b31;
-  background-color: #D49C6B;
+  background-color: #d49c6b;
   border-radius: 1rem;
-  padding-inline: .4rem;
-  margin-right: .4rem;
+  padding: 0 0.5rem 0.2rem 0.5rem;
+  margin-right: 0.4rem;
+}
+.buscador-card--filtro {
+  padding: .6rem;
+  min-height: 10rem;
+  max-height: none;
+  border-radius: 1rem;
+  box-shadow: 1.5px 1.5px 30px -10px #252b31;
+  background-color: #eaedee;
+}
+.badge-container--filtro {
+}
+.badge-custom--filtro {
+  color: #d49c6b;
+  background-color: #252b31;
 }
 .box2 {
   background-color: #eaedee;
