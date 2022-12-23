d62068254d12d3e3ab9efdcea3eccfe12eb5f690
pjdu39 <pjdu39@gmail.com>
2022-12-23 12:56:33 +0100

    Encuestas y Discusiones en el Tablón

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,24 @@
             :key="index"
           >
           <div class="linea-superior">
-            <h4>{{ elemento.titulo }}</h4>
+            <div class="tipo-elemento">{{ elemento.tipo }}</div>
             <div class="autor">por <b>{{ elemento.autor }}</b></div>
           </div>
+          <h4 class="titulo">{{ elemento.titulo }}</h4>
           <div>{{ elemento.descripcion }}</div>
+          <div 
+              v-if="elemento.tipo === 'Encuesta'" 
+              class="encuesta-container"
+            >
+            <div v-for="opcion in elemento.opciones" :key="opcion.id">
+              <div class="encuesta-opcion">
+                <div class="encuesta-opcion-texto">{{ opcion.texto }}</div>
+                <!--<div class="encuesta-opcion-barra" :style="'width: 2rem'"></div>-->
+                <div class="encuesta-opcion-barra" :style="'width: ' + calculaBarraEncuesta(elemento.opciones, opcion) + 'rem;'"></div>
+                <div class="encuesta-opcion-barra encuesta-opcion-barra--fondo"></div>
+              </div>
+            </div>
+          </div>
           <div class="interacciones">
             <div class="likes">
               <span class="fa fa-heart" aria-hidden="true"></span>
@@ -37,9 +51,10 @@ export default {
     return {
       elementos: [
         { 
+          tipo: 'Receta',
           titulo: 'Cocido',
           descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-          autor: 'Carlos',
+          autor: 'carlos',
           likes: 67,
           comentarios: [
             { usuario: 'Pepe Hamond', texto: 'Lorem ipsum dolor sit amet' },
@@ -47,17 +62,53 @@ export default {
           ]
         },
         { 
+          tipo: 'Receta',
           titulo: 'Galletas',
           descripcion: 'Lorem ipsum dolor sit amet adipisicing elit',
-          autor: 'Andrea',
+          autor: 'andrea',
           likes: 216,
           comentarios: [
             { usuario: 'Pepe Hamond', texto: 'Consectetur adipisicing elit. Dolor sit amet ipsum dolor sit consectetur adipisicing hahah' },
             { usuario: 'Carlos', texto: 'Lorem ipsum dolor sit amet.' },
             { usuario: 'Andrea', texto: 'Consectetur elit adipisicing. Dolor sit amet consectetur adipisicing' }
           ]
+        },
+        { 
+          tipo: 'Discusión',
+          titulo: 'Cilantro o no?',
+          descripcion: 'Qué opinais del uso del cilantro en la cocina latino americana?',
+          autor: 'sara_rivas_',
+          likes: 13,
+          comentarios: [
+            { usuario: 'Pepe Hamond', texto: 'Lorem ipsum dolor sit consectetur adipisicing hahah. sit amet.' },
+            { usuario: 'Andrea', texto: 'Consectetur elit adipisicing. Dolor sit amet consectetur adipisicing' }
+          ]
+        },
+        { 
+          tipo: 'Encuesta',
+          titulo: 'Cocido con o sin patata?',
+          descripcion: null,
+          autor: 'misco_jones',
+          likes: 693,
+          comentarios: [],
+          opciones: [
+            { id: 1, texto: 'Con patata', votos: 319 },
+            { id: 2, texto: 'Sin patata', votos: 856 }
+          ]
         }
       ]
+    }
+  },
+  methods: {
+    calculaBarraEncuesta(encuestaOpciones, opcion) {
+      let votosTotales = encuestaOpciones.reduce(
+        (acum, x) => acum + x.votos, 0
+      )
+      let porcentaje = opcion.votos / votosTotales * 100
+      
+      console.log(Math.trunc(porcentaje))
+
+      return Math.trunc(porcentaje / 5)
     }
   }
 }
@@ -77,15 +128,73 @@ export default {
   box-shadow: 1.5px 1.5px 30px -10px #252b31;
 }
 
+.tipo-elemento {
+  display: inline-block;
+  color: white;
+  background-color: #C1C8C7;
+  font-size: 110%;
+  font-weight: 400;
+  letter-spacing: .3rem;
+  margin-left: -.3rem;
+  padding: 0 .6rem 0.1rem .6rem;
+  border-radius: 2rem;
+  /* text-align: center; */
+}
+
 .linea-superior {
   display: inline-flex;
   width: 100%;
+  margin-bottom: 1.4rem;
+}
+
+.titulo {
+  margin-bottom: 1rem;
 }
 
 .autor {
   text-align: end;
   margin-left: auto;
   font-style: italic;
+}
+
+.encuesta-container {
+ margin-bottom: 1rem;
+}
+
+.encuesta-opcion {
+  position: relative;
+  /* width: 20rem; */
+  height: 1.6rem;
+  /* display: inline-flex; */
+  margin: 0 0 .8rem 0;
+}
+
+.encuesta-opcion-texto {
+  display: inline-block;
+  position: sticky; /* TODO: Ver qué hace esto y cambiar si es necesario. Ahora está puesto para evitar que el valor por defecto impida que se aploque el z-index */
+  margin-left: .5rem;
+  margin-top: .1rem;
+  font-size: 85%;
+  z-index: 10;
+}
+
+.encuesta-opcion-barra {
+  position: absolute;
+  /* width: 12rem; */
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: #d49c6b;
+  border-radius: .5rem 0 0 .5rem;
+  padding: 0 0 0 1rem;
+  z-index: 5;
+}
+
+.encuesta-opcion-barra--fondo {
+  width: 20rem;
+  background-color: #C1C8C7;
+  border-radius: 0.5rem;
+  z-index: 1;
 }
 
 .interacciones {
