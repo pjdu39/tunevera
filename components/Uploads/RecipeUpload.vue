3ee1167867fee1edcf322c8d62bb0f152d4c247d
pjdu39 <pjdu39@gmail.com>
2024-06-22 15:53:18 +0200

    Ajuste de calidad basado en el peso en vez de en resolución

--- components/Uploads/RecipeUpload.vue
+++ components/Uploads/RecipeUpload.vue
@@ -439,7 +439,7 @@ const nuxtImgClass = computed(() => {
   if (finalBlob.value) return "image-fit";
   else return "image-fit image-empty";
 });
-
+/*
 const resizeImage = async (file, maxWidth = 500, maxHeight = 500) => {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -464,6 +464,10 @@ const resizeImage = async (file, maxWidth = 500, maxHeight = 500) => {
         canvas.height = height;
 
         const ctx = canvas.getContext("2d");
+
+        ctx.imageSmoothingEnabled = true;
+        ctx.imageSmoothingQuality = 'high'; // Opciones: 'low', 'medium', 'high'
+
         ctx.drawImage(img, 0, 0, width, height);
 
         canvas.toBlob(
@@ -472,6 +476,60 @@ const resizeImage = async (file, maxWidth = 500, maxHeight = 500) => {
           },
           file.type,
           1
+        );
+      };
+
+      img.onerror = (err) => {
+        reject(err);
+      };
+    };
+
+    reader.onerror = (err) => {
+      reject(err);
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
+*/
+const resizeImage = async (file, targetSizeKB = 700) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      img.src = e.target.result;
+
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        const ctx = canvas.getContext("2d");
+        ctx.imageSmoothingEnabled = true;
+        ctx.imageSmoothingQuality = 'high';
+
+        // Dibujar la imagen original en el canvas
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+
+        // Convertir inicialmente la imagen a Blob para obtener su tamaño inicial
+        canvas.toBlob(
+          (initialBlob) => {
+            const initialSizeKB = initialBlob.size / 1024;
+            // Calcular la calidad necesaria usando una regla de tres
+            const quality = Math.max(0, Math.min(1, targetSizeKB / initialSizeKB));
+
+            // Convertir la imagen a Blob con la calidad calculada
+            canvas.toBlob(
+              (finalBlob) => {
+                resolve(finalBlob);
+              },
+              file.type,
+              quality
+            );
+          },
+          file.type,
+          1 // Máxima calidad para medir el tamaño inicial
         );
       };
 
